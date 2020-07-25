11fa077d5c2b96ecda6ee2f0291edb53c17170d0
loverdom <58025497+loverdom@users.noreply.github.com>
2020-07-25 23:25:26 +0300

    Update quick_sort.py

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,10 +1,11 @@
 import sys
+from random import randint
 
 def qsort(arr):
     if len(arr) < 2:
         return arr
     else:
-        root = arr[0]
+        root = arr[randint(0,(len(arr) - 1))]
         less = [i for i in arr[1:] if i <= root]
         greater = [i for i in arr[1:] if i > root]
         return qsort(less) + [root] + qsort(greater)
